45a03f1996d90b9e77761dfffdaee4df877f8318
AdnanfadhilYaser <sandirumit123@gmail.com>
2025-08-20 14:30:59 +0700

    Adding docker image schema and zookeper

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -7,13 +7,41 @@ default_args = {
     'start_date': datetime(2023,9,3,10,00),
 }
 
+def get_data():
+    import requests
+
+    res = requests.get("http://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+    # print(res)
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = (f"{str(location['street']['number'])}{location['street']['name']}"
+                       f"{location['city']}{location['state']}{location['country']}")
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['id'] = res['id']['name']
+    data['picture'] = res['picture']['medium']
+
+    return data
+    
 def stream_data():
     import json
-    import requests
-    res = requests.get("https://randomuser.me/api/")
-    res = res.json()
-    print(res)
-    res = res["results"][0]
+
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
+
 
 with DAG('user_automation',
          default_args=default_args,
